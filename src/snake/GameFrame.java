fdcdcee78610de0175f13ebe205581b7fda0cffc
d4vidrozsa <d4vidrozsa@gmail.com>
2022-08-19 13:03:33 +0200

    version 1

---
+++ src/snake/GameFrame.java
@@ -0,0 +1,16 @@
+package snake;
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+
+	public GameFrame(){
+		
+		this.add(new GamePanel());
+		this.setTitle("Snek");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.pack();
+		this.setVisible(true);
+		this.setLocationRelativeTo(null);
+	}
+}
